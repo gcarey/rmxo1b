c445d68d648eab8826283ae7c89006aaa443ab28
Garrett Carey <garretticarey@gmail.com>
2014-12-03 13:56:56 -0500

    v.0.0.0.9 Loading state on send button

--- main.js
+++ main.js
@@ -229,10 +229,11 @@ var userLoader = (function() {
         $("#field").focus()
       });
 
-      $( '#send-button' ).click(function() {
+      $( '#send_button' ).click(function() {
         sendTip(siteUrl,
-                userID,
-                recipients.join(","));
+          userID,
+          recipients.join(","));
+        this.innerHTML = 'Sending<span>...</span>';
       });
     }
   }
@@ -245,22 +246,14 @@ var userLoader = (function() {
     return str.length < max ? pad("0" + str, max) : str;
   }
 
-  function runSpinner() {
+  function loadSpinner() {
     var opts = {
       lines: 11, // The number of lines to draw
       length: 5, // The length of each line
       width: 2, // The line thickness
       radius: 5, // The radius of the inner circle
       corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      direction: 1, // 1: clockwise, -1: counterclockwise
-      color: '#000', // #rgb or #rrggbb or array of colors
-      speed: 1, // Rounds per second
-      trail: 60, // Afterglow percentage
-      shadow: false, // Whether to render a shadow
-      hwaccel: false, // Whether to use hardware acceleration
       className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
       top: '65%', // Top position relative to parent
       left: '50%' // Left position relative to parent
     };
@@ -268,12 +261,10 @@ var userLoader = (function() {
     spinner = new Spinner(opts).spin(target);
   }
 
-
-
   // Event handlers
   return {
     onload: function () {
-      runSpinner();
+      loadSpinner();
       getInfo();
     }
   };
